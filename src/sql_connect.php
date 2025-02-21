06688951abd81e13aa84066fc796db1fce868bff
osaviuk <osaviuk@ebsco.com>
2025-02-21 13:13:06 +0200

    math test fixes and cleanup

--- src/sql_connect.php
+++ src/sql_connect.php
@@ -3,10 +3,6 @@ $servername = getenv('MYSQL_SERVER');
 $username = getenv('MYSQL_USER');
 $password = getenv('MYSQL_PASSWORD');
 $dbname = getenv('MYSQL_DATABASE');
-// $servername = "db";
-// $username = "user";
-// $password = "userpassword";
-// $dbname = "test_db";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
